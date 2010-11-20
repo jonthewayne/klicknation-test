c997b30d299e52fd3e0955b9d8252ec84dd16c94
Jon Shumate <jonthewayne@gmail.com>
2010-11-20 03:49:07 -0800

    first commit for klicknation test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlicknationTest::Application.config.secret_token = 'a28f732cca65169dfe3a978ae96ee084050ddeaeb75aade65b8ffa7a34da787e0f9dc4e4662d9879d65e538fcab712df4a0b7d2b7a667fc8a3f9d1e32d8d70d2'
