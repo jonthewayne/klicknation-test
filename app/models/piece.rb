84094357b96ce05ec4de7970f07fb87f13a41f70
Jon Shumate <jonthewayne@gmail.com>
2010-11-21 03:39:48 -0800

    adding fields, validations, and updates to the views

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,5 +1,15 @@
 class Piece
   include Mongoid::Document
-  field :name, :type => String
-  field :description, :type => String
+  field :name
+  field :description
+  field :attack, :type => Integer
+  field :defense, :type => Integer 
+  field :movement, :type => Integer
+  field :cost, :type => Integer
+  
+  validates_presence_of :name
+  validates_numericality_of :attack, :only_integer => true, :message => "can only be whole number."
+  validates_numericality_of :defense, :only_integer => true, :message => "can only be whole number."
+  validates_numericality_of :movement, :only_integer => true, :message => "can only be whole number."
+  validates_numericality_of :cost, :only_integer => true, :message => "can only be whole number."
 end
